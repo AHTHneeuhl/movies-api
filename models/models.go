e2fe0e233158d43a2442577b792f05af6a6e6f9d
Shubham Patrick <ahthneeuhl@gmail.com>
2023-06-25 19:16:22 +0530

    chore: create Movie, Genre and MovieGenre model types

---
+++ models/models.go
@@ -0,0 +1,33 @@
+package models
+
+import "time"
+
+type Movie struct {
+	ID          int          `json:"id"`
+	Title       string       `json:"title"`
+	Description string       `json:"description"`
+	Year        int          `json:"year"`
+	ReleaseDate time.Time    `json:"releaseDate"`
+	Runtime     int          `json:"runtime"`
+	Rating      int          `json:"rating"`
+	MPAARating  string       `json:"mppaRating"`
+	CreatedAt   time.Time    `json:"createdAt"`
+	UpdatedAt   time.Time    `json:"updatedAt"`
+	MovieGenre  []MovieGenre `json:"-"`
+}
+
+type Genre struct {
+	ID        int       `json:"id"`
+	GenreName string    `json:"genreName"`
+	CreatedAt time.Time `json:"createdAt"`
+	UpdatedAt time.Time `json:"updatedAt"`
+}
+
+type MovieGenre struct {
+	ID        int       `json:"id"`
+	MovieID   int       `json:"movieId"`
+	GenreID   int       `json:"genreId"`
+	Genre     Genre     `json:"genre"`
+	CreatedAt time.Time `json:"createdAt"`
+	UpdatedAt time.Time `json:"updatedAt"`
+}
